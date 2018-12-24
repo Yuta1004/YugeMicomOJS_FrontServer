59a5fe5d0ced3c1d7082c7d03c919a9d94ff98a7
NakagamiYuta <guguru0014@gmail.com>
2018-12-24 19:57:28 +0900

    [add]ユーザのログイン状態をセッションを用いて管理するように仮実装

--- judge_front_main.py
+++ judge_front_main.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_bootstrap import Bootstrap
 from login_process import register
 
 # Flask
 app = Flask(__name__)
+app.config.from_pyfile("config.cfg")
 bootstrap = Bootstrap(app)
 
 # Other
 base_url = "/yuge_micom_ojs"
 
 #Routes
+@app.before_request
+def before_request():
+    if "user_id" not in session.keys():
+        session["user_id"] = None
+
+
 @app.route(base_url + "/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",
+                           session=session["user_id"])
 
 
 @app.route(base_url + "/register", methods=["GET", "POST"])
 def register_user():
     if request.method == "GET":
-        return render_template("register.html")
+        return render_template("register.html",
+                               session=session["user_id"])
 
     user_id = request.form["user_id"]
     user_name = request.form["user_name"]
@@ -28,13 +37,16 @@ def register_user():
     if register(user_id, user_name, password, password_conf):
         return "Register Successful"
     else:
-        return render_template("register.html", inp_failed="Failed")
+        return render_template("register.html",
+                               inp_failed="Failed",
+                               session=session["user_id"])
 
 
 @app.route(base_url + "/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+        return render_template("login.html",
+                               session=session["user_id"])
 
     return "None"
 
