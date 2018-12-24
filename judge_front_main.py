9f75c7ee30e9bae2f6b1495661bfc3ef80b06b7f
NakagamiYuta <guguru0014@gmail.com>
2018-12-24 17:18:48 +0900

    [change]コメント追加・微調整

--- judge_front_main.py
+++ judge_front_main.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
+# Flask
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+# Other
 base_url = "/judge_system"
 
+#Routes
 @app.route(base_url + "/")
 def index():
     return render_template("index.html")
 
 
 @app.route(base_url + "/register", methods=["GET", "POST"])
-def login_user():
+def register_user():
     if request.method == "GET":
         return render_template("register.html")
 
