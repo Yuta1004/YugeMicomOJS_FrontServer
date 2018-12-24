d4fcc97a947c5387f44320ec4403aff0f46e13e2
NakagamiYuta <guguru0014@gmail.com>
2018-12-24 23:31:29 +0900

    [add]過去、今、未来のコンテストをそれぞれまとめて返してくれる関数を作成した

--- contest.py
+++ contest.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 class ContestInfo:
     def __init__(self, _id, name, start, end):
@@ -13,11 +14,32 @@ def get_all_contest():
     cur = connect.cursor()
 
     all_contest = []
+    time_format = "%Y/%m/%d/%H/%M"
     cur.execute("SELECT * FROM contest");
     for contest in cur.fetchall():
         all_contest.append(ContestInfo(contest[0],
                                        contest[1],
-                                       contest[2],
-                                       contest[3]))
+                                       datetime.strptime(contest[2], time_format),
+                                       datetime.strptime(contest[3], time_format)))
+
     return all_contest
+
+
+def get_3type_divided_contest():
+    now = datetime.now()
+    past_contest = []
+    now_contest = []
+    future_contest = []
+
+    for contest in get_all_contest():
+        if contest.start_time <= now <= contest.end_time:
+            now_contest.append(contest)
+
+        elif contest.end_time < now:
+            past_contest.append(contest)
+
+        else:
+            future_contest.append(contest)
+
+    return past_contest, now_contest, future_contest
 
