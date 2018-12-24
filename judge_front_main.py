45a88468f38a197382af83b9225d4f0b5c86f68b
NakagamiYuta <guguru0014@gmail.com>
2018-12-24 16:45:34 +0900

    [add]フォームからPOSTされた情報を取り出す処理を書いた

--- judge_front_main.py
+++ judge_front_main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -9,6 +9,19 @@ base_url = "/judge_system"
 @app.route(base_url + "/")
 def index():
     return render_template("index.html")
+
+
+@app.route(base_url + "/register", methods=["GET", "POST"])
+def login_user():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    user_id = request.form["user_id"]
+    user_name = request.form["user_name"]
+    password = request.form["password"]
+    password_conf = request.form["password_conf"]
+
+    return ""
 
 
 if __name__ == '__main__':
