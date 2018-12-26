866bd3755ae224a112035baa870986cf15f4e997
NakagamiYuta <guguru0014@gmail.com>
2018-12-26 18:19:33 +0900

    [change]コンテスト一覧ページのURLを変更

--- judge_front_main.py
+++ judge_front_main.py
@@ -71,18 +71,21 @@ def logout_user():
     return redirect(base_url)
 
 
+@app.route(base_url + "/contest_list")
+def contest_list_view():
+    now_page = request.args.get("page", 1, type=int)
+    past_contest, now_contest, future_contest = get_3type_divided_contest()
+
+    return render_template("contest_list.html",
+                            session=session["user_id"],
+                            past_contest=past_contest,
+                            now_contest=now_contest,
+                            now_page=now_page,
+                            future_contest=future_contest)
+
+
 @app.route(base_url + "/contest/<path:contest_id>")
 def contest_view(contest_id):
-    if contest_id == "all":
-        now_page = request.args.get("page", 1, type=int)
-        past_contest, now_contest, future_contest = get_3type_divided_contest()
-        return render_template("contest_list.html",
-                                session=session["user_id"],
-                                past_contest=past_contest,
-                                now_contest=now_contest,
-                                now_page=now_page,
-                                future_contest=future_contest)
-
     return contest_id
 
 
