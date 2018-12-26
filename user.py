8f852d4c34e74cb2ad39ce2a86ce0dd84eb3960c
NakagamiYuta <guguru0014@gmail.com>
2018-12-26 19:25:07 +0900

    [add]ユーザデータを更新する関数を作成

--- user.py
+++ user.py
@@ -23,3 +23,18 @@ def get_user_data(user_id):
     result = result[0]
     return UserInfo(result[0], result[1], True if result[2] == 1 else False)
 
+
+def update_user_data(user_id, user_name, open_code):
+    connect = sqlite3.connect("DB/user.db")
+    cur = connect.cursor()
+
+    cur.execute("UPDATE auth_info SET name = ? WHERE id = ?",
+                (user_name, user_id))
+    connect.commit()
+
+    cur.execute("UPDATE settings SET open_code = ? WHERE id = ?",
+                (open_code, user_id))
+    connect.commit()
+
+    cur.close()
+    connect.close()
