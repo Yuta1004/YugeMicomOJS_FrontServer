fd80d569fc3a7bf24b673e8bc2e61ef583ba33ed
NakagamiYuta <guguru0014@gmail.com>
2019-01-02 15:27:21 +0900

    [change]ファイルチェックとパスワードチェックをまとめて関数にした

--- file_read.py
+++ file_read.py
@@ -1,12 +1,20 @@
 import os
 
 
-def get_code(submission_id, password):
+def get_file_check(password, path):
     if password != os.environ["JUDGE_SYSTEM_PASSWORD"]:
+        return False
+
+    if not os.path.exists(path):
+        return False
+
+    return True
+
+
+def get_code(submission_id, password):
+    if not get_file_check(password, "Submission/" + submission_id + ".txt"):
         return ""
 
-    if not os.path.exists("Submission/" + submission_id + ".txt"):
-        return ""
 
     with open("Submission/" + submission_id + ".txt", "r", encoding="utf-8") as f:
         return f.read()
