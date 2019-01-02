215eb6ceaf55db408a660db52aab863d0583ad54
NakagamiYuta <guguru0014@gmail.com>
2019-01-02 22:20:12 +0900

    [add]判定イメージ起動→DB反映までの判定処理を行う関数を作成

---
+++ judge.py
@@ -0,0 +1,53 @@
+import docker
+import os
+import sqlite3
+from collections import Counter
+from configparser import ConfigParser
+
+
+# Config
+config_file = ConfigParser()
+config_file.read("config.ini")
+
+
+def judge_code(submission_id, problem_id):
+    client = docker.from_env()
+
+    image_name = config_file["docker"]["image_name"]
+    env = {"LD_LIBRARY_PATH": "/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64"}
+    commands = [
+        "judge-program",
+        config_file["system"]["server_url"],
+        config_file["system"]["password"],
+        submission_id,
+        problem_id
+    ]
+
+    judge_result = client.containers.run(image_name, commands, remove=True, environment=env)
+
+    # 判定取り出し
+    judge_list = []
+    for line in judge_result.decode().split("\n")[:-1]:
+        judge_list.append(line.split("`")[1])
+
+    if judge_result.decode == "":
+        judge_list = ["IE"]
+
+    # 全体の判定を決める
+    judge_counter = Counter(judge_list)
+    judge_status = judge_counter.most_common()[0][0]
+    if judge_status == "AC" and len(judge_list) != judge_counter["AC"]:
+        judge_status = judge_counter.most_common()[1][0]
+
+    # 提出データ更新
+    connect = sqlite3.connect("DB/problem.db")
+    cur = connect.cursor()
+
+    judge_status_id = cur.execute("SELECT id FROM status WHERE name = ?", (judge_status, )).fetchone()[0]
+    sql = "UPDATE submission SET status = ?, detail = ? WHERE id = ?"
+    cur.execute(sql, (judge_status_id, judge_result.decode().replace("\n", ";"), submission_id))
+    connect.commit()
+
+    cur.close()
+    connect.close()
+
