4a48498378fbcb2476aeea793e8d723c8caa79d8
NakagamiYuta <guguru0014@gmail.com>
2019-01-03 23:51:42 +0900

    [fix]コード公開の判定が上手く動作していなかったので修正

--- submission.py
+++ submission.py
@@ -1,5 +1,6 @@
 import sqlite3
 import uuid
+from datetime import datetime
 from judge import add_judge_job
 
 
@@ -57,13 +58,14 @@ class SubmissionDetail:
         self.status = status
         self.err_msg = err_msg
 
-def get_data_for_submission_page(submission_id):
+def get_data_for_submission_page(user_id, submission_id):
     # 提出詳細取得
     connect = sqlite3.connect("DB/problem.db")
     cur = connect.cursor()
 
     sql = """
-          SELECT submission.id, problem.id, problem.name, submission.user_id, submission.date, submission.lang, status.name, submission.detail
+          SELECT submission.id, problem.id, problem.name, submission.user_id,
+                    submission.date, submission.lang, status.name, submission.detail, problem.open_time
           FROM submission, status
           INNER JOIN problem ON submission.problem_id = problem.id
           WHERE submission.status = status.id AND submission.id = ?
@@ -74,7 +76,9 @@ def get_data_for_submission_page(submission_id):
                                      fetch_result[4], fetch_result[5],
                                      fetch_result[6], fetch_result[7])
 
-    user_id = fetch_result[3]
+    # 必要情報取得
+    submission_user_id = fetch_result[3]
+    open_time = fetch_result[8]
     cur.close()
     connect.close()
 
@@ -96,10 +100,12 @@ def get_data_for_submission_page(submission_id):
     connect = sqlite3.connect("DB/user.db")
     cur = connect.cursor()
     open_code = cur.execute("SELECT open_code FROM settings WHERE id = ?", (user_id, )).fetchone()[0]
-    cur.close()
-    connect.close()
+    time_format = "%Y-%m-%d %H:%M:%S"
+    is_open_code = (open_code == 1 and datetime.strptime(open_time, time_format) <= datetime.now()) \
+                        or user_id == submission_user_id
 
-    return submission_data, submission_code, open_code == 1
+    return submission_data, submission_code, is_open_code
+
 
 def save_submission(user_id, problem_id, lang, code):
     connect = sqlite3.connect("DB/problem.db")
