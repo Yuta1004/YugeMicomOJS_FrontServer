44e28b0c194c3067de40c764464cc7739d75efdf
NakagamiYuta <guguru0014@gmail.com>
2019-02-07 23:55:49 +0900

    [fix]ページ表示前のリダイレクトが無効化される不具合を修正
    - Blueprintでbeforerequestを定義したらだめ

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session, request, redirect, render_template
 from flask_bootstrap import Bootstrap
 from configparser import ConfigParser
 
@@ -15,10 +15,29 @@ app = Flask(__name__, static_url_path='/yuge_micom_ojs/static')
 app.config["SECRET_KEY"] = config_file["system"]["password"]
 bootstrap = Bootstrap(app)
 
+# BeforeRequestRoute
+@app.before_request
+def before_request():
+    if "user_id" not in session.keys():
+       session["user_id"] = None
+
+    enough_login = True
+    for url in no_login_ok_url:
+        if url in request.url:
+            enough_login = False
+
+    if enough_login and session["user_id"] is None:
+        return redirect(base_url + "/login")
+
+# ErrorHandler
+@app.errorhandler(404)
+def error_404_notfound(error):
+    return render_template("404.html",
+                           session=session["user_id"])
+
 # View
 from server.views.route_auth import route_auth
 from server.views.route_contest import route_contest
-from server.views.route_error import route_error
 from server.views.route_judge_process import route_judge_process
 from server.views.route_problem import route_problem
 from server.views.route_submission import route_submission
@@ -27,7 +46,6 @@ from server.views.route_user import route_user
 
 app.register_blueprint(route_auth)
 app.register_blueprint(route_contest)
-app.register_blueprint(route_error)
 app.register_blueprint(route_judge_process)
 app.register_blueprint(route_problem)
 app.register_blueprint(route_submission)
--- server/views/route_top.py
+++ server/views/route_top.py
@@ -5,21 +5,6 @@ from server import base_url
 
 route_top = Blueprint(__name__, "top")
 
-# リクエストが飛んできたらまずここに来る
-@route_top.before_request
-def before_request():
-    if "user_id" not in session.keys():
-       session["user_id"] = None
-
-    # ログインが必要なURLかどうか判定
-    enough_login = True
-    for url in no_login_ok_url:
-        if url in request.url:
-            enough_login = False
-
-    if enough_login and session["user_id"] is None:
-        return redirect(base_url + "/login")
-
 # トップページ
 @route_top.route(base_url + "/")
 def index():
