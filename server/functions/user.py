99abbe368ba1fa3c8a813eb30bc12ec5f2505fa3
NakagamiYuta <guguru0014@gmail.com>
2019-02-08 16:16:23 +0900

    [clean]user.pyにdocstringを追加

--- server/functions/user.py
+++ server/functions/user.py
@@ -3,7 +3,21 @@ import bcrypt
 from server.functions.login_process import login
 
 class UserInfo:
+    """ユーザデータを扱うデータクラス"""
+
     def __init__(self, _id, name, position, open_code):
+        """コンストラクタ
+
+        Args:
+            _id (str) : ユーザID
+            name (str) : ユーザ名/ニックネーム
+            position (str) : [admin]で管理者、それ以外は一般ユーザ
+            open_code (str) : コード公開設定
+
+        Returns:
+            None
+        """
+
         self.id = _id
         self.name = name
         self.position = position
@@ -11,14 +25,23 @@ class UserInfo:
 
 
 def get_user_data(user_id):
-    connect = sqlite3.connect("./server/DB/user.db")
-    cur = connect.cursor()
+    """指定IDのユーザデータを返す
+
+    Args:
+        user_id (str) : ユーザID
+
+    Returns:
+        UserInfo : ユーザデータ
+    """
 
     sql = "SELECT auth_info.id, auth_info.name, auth_info.position, settings.open_code \
            FROM auth_info \
            INNER JOIN settings ON auth_info.id = settings.id \
            WHERE auth_info.id = ?"
 
+    # データ取得
+    connect = sqlite3.connect("./server/DB/user.db")
+    cur = connect.cursor()
     result = cur.execute(sql, (user_id, )).fetchall()
     if len(result) != 1:
         return None
@@ -31,19 +54,27 @@ def get_user_data(user_id):
 
 
 def update_user_data(user_id, user_name, open_code):
-    connect = sqlite3.connect("./server/DB/user.db")
-    cur = connect.cursor()
+    """指定IDのユーザデータを更新する
 
+    Args:
+        user_id (str) : ユーザID
+        user_name (str) : ユーザ名/ニックネーム
+        open_code (int) : コード公開設定、1を指定すると公開するになる
+
+    Returns:
+        bool : 更新に成功したらTrue
+    """
     if user_name == "":
         return False
 
     # 更新処理
+    connect = sqlite3.connect("./server/DB/user.db")
+    cur = connect.cursor()
     cur.execute("UPDATE auth_info SET name = ? WHERE id = ?",
                 (user_name, user_id))
     cur.execute("UPDATE settings SET open_code = ? WHERE id = ?",
                 (open_code, user_id))
     connect.commit()
-
     cur.close()
     connect.close()
 
@@ -51,11 +82,20 @@ def update_user_data(user_id, user_name, open_code):
 
 
 def change_password(user_id, old_pass, new_pass, new_pass_conf):
+    """指定ユーザのパスワードを変更する
+
+    Args:
+        user_id (str) : ユーザID
+        old_pass (str) : 旧パスワード
+        new_pass (str) : 新パスワード
+        new_pass_conf (str) : 新パスワード、入力確認用
+
+    Returns:
+        bool : 正常に変更されたらTrue
+    """
+
     if new_pass != new_pass_conf or not login(user_id, old_pass):
         return False
-
-    connect = sqlite3.connect("./server/DB/user.db")
-    cur = connect.cursor()
 
     # 半角英数8文字以上
     if len(new_pass) < 8 or not new_pass.encode('utf-8').isalnum():
@@ -66,10 +106,11 @@ def change_password(user_id, old_pass, new_pass, new_pass_conf):
     hash_password = bcrypt.hashpw(new_pass.encode(), gen_salt)
 
     # パスワード変更
+    connect = sqlite3.connect("./server/db/user.db")
+    cur = connect.cursor()
     cur.execute("UPDATE auth_info SET password = ? WHERE id = ?",
                 (hash_password.decode(), user_id))
     connect.commit()
-
     cur.close()
     connect.close()
 
@@ -77,9 +118,17 @@ def change_password(user_id, old_pass, new_pass, new_pass_conf):
 
 
 def is_admin(user_id):
+    """指定IDのユーザが管理者かどうかを返す
+
+    Args:
+        user_id (str) : ユーザID
+
+    Returns:
+        bool : 管理者の場合はTtue
+    """
+
     connect = sqlite3.connect("./server/DB/user.db")
     cur = connect.cursor()
-
     position = cur.execute("SELECT position FROM auth_info WHERE id = ?",
                            (user_id, )).fetchone()[0]
     cur.close()
