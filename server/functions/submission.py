673d21dc132376d707667266eb4cc3fb2eb75f6e
NakagamiYuta <guguru0014@gmail.com>
2019-05-04 16:23:03 +0900

    [add]提出記録にコード実行時間を表示するようにした

--- server/functions/submission.py
+++ server/functions/submission.py
@@ -7,7 +7,8 @@ from server.functions.judge import add_judge_job
 class SubmissionInfo:
     """提出情報を扱うデータクラス"""
 
-    def __init__(self, submission_id, problem_id, problem_name, open_time, user_id, user_name, date, lang, status, detail, score):
+    def __init__(self, submission_id, problem_id, problem_name, open_time,
+                 user_id, user_name, date, lang, status, detail, score, exec_time):
         """コンストラクタ
 
         Args:
@@ -22,6 +23,7 @@ class SubmissionInfo:
             status (str) : ジャッジステータス
             detail (SubmissonDetail) : 実行詳細
             score (str) : スコア
+            exec_time (float) : コード実行時間
 
         Returns:
             None
@@ -38,6 +40,11 @@ class SubmissionInfo:
         self.status = status
         self.detail = detail
         self.score = score
+        self.exec_time = float(exec_time)
+
+        if self.exec_time >= 0:
+            self.exec_time *= 1000
+            self.exec_time = int(self.exec_time)
 
 
 def get_submission_data(user_id, problem_id):
@@ -59,10 +66,14 @@ def get_submission_data(user_id, problem_id):
     cur.execute("ATTACH \"./server/DB/user.db\" AS user")
 
     # SQL
-    sql_base = "SELECT submission.id, problem.id, problem.name, problem.open_time, submission.user_id, user.auth_info.name, submission.date, submission.lang, status.name, submission.detail, submission.score\
+    sql_base =  """
+                SELECT submission.id, problem.id, problem.name, problem.open_time,
+                       submission.user_id, user.auth_info.name, submission.date,
+                       submission.lang, status.name, submission.detail, submission.score, submission.exec_time\
                 FROM submission, status, user.auth_info \
                 INNER JOIN problem ON submission.problem_id = problem.id \
-                WHERE submission.status = status.id AND submission.user_id = user.auth_info.id"
+                WHERE submission.status = status.id AND submission.user_id = user.auth_info.id
+                """
 
     sql_base_sort = " ORDER BY submission.date DESC"
 
@@ -92,13 +103,14 @@ def get_submission_data(user_id, problem_id):
 class SubmissionDetail:
     """提出情報詳細を扱うデータクラス"""
 
-    def __init__(self, test_case_name, status, err_msg):
+    def __init__(self, test_case_name, status, exec_time, err_msg):
         """コンストラクタ
 
         Args:
             test_case_name (str) : テストケース名
             status (str) : ジャッジステータス
             err_msg (str) : 実行詳細
+            exec_time (float) : 実行時間
 
         Returns:
             None
@@ -107,6 +119,12 @@ class SubmissionDetail:
         self.test_case_name = test_case_name
         self.status = status
         self.err_msg = err_msg
+        self.exec_time = float(exec_time)
+
+        if self.exec_time >= 0:
+            self.exec_time *= 1000
+            self.exec_time = int(self.exec_time)
+
 
 def get_data_for_submission_page(user_id, submission_id):
     """提出詳細ページを描画するために必要な情報を返す
@@ -127,18 +145,20 @@ def get_data_for_submission_page(user_id, submission_id):
     cur.execute("ATTACH \"./server/DB/user.db\" AS user")
 
     sql = """
-          SELECT submission.id, problem.id, problem.name, problem.open_time, submission.user_id, user.auth_info.name,
-                    submission.date, submission.lang, status.name, submission.detail, submission.score, problem.open_time
+          SELECT submission.id, problem.id, problem.name, problem.open_time,
+                 submission.user_id, user.auth_info.name, submission.date,
+                 submission.lang, status.name, submission.detail, submission.score,
+                 submission.exec_time, problem.open_time
           FROM submission, status, user.auth_info
           INNER JOIN problem ON submission.problem_id = problem.id
           WHERE submission.status = status.id AND submission.id = ? AND user.auth_info.id = submission.user_id
           """
     fetch_result = cur.execute(sql, (submission_id, )).fetchone()
-    submission_data = SubmissionInfo(*fetch_result[:11])
+    submission_data = SubmissionInfo(*fetch_result[:12])
 
     # 必要情報取得
     submission_user_id = fetch_result[4]
-    open_time = fetch_result[11]
+    open_time = fetch_result[12]
     cur.close()
     connect.close()
 
@@ -147,9 +167,19 @@ def get_data_for_submission_page(user_id, submission_id):
     detail = [item.split("`n`") for item in detail]
     detail_data = {}
     for idx, elem in enumerate(detail[:-1]):
-        detail_data[str(idx)] = SubmissionDetail(elem[0],   # テストケース名
-                                                 elem[1],   # ジャッジステータス
-                                                 elem[2].replace("`n2`", "\n").replace("/tmp/judge/src", "/path/to/code"))
+        if len(elem) == 5:
+            if elem[2] == "timeout":
+                elem[2] = -1.0
+            detail_data[str(idx)] = SubmissionDetail(elem[0],   # テストケース名
+                                                     elem[1],   # ジャッジステータス
+                                                     elem[2],   # 実行時間
+                                                     elem[3].replace("`n2`", "\n").replace("/tmp/judge/src", "/path/to/code"))
+        else:
+            detail_data[str(idx)] = SubmissionDetail(elem[0],   # テストケース名
+                                                     elem[1],   # ジャッジステータス
+                                                     0.0,       # 実行時間
+                                                     elem[2].replace("`n2`", "\n").replace("/tmp/judge/src", "/path/to/code"))
+
     submission_data.detail = detail_data
 
     # 提出コード取得
@@ -188,7 +218,7 @@ def save_submission(user_id, problem_id, lang, code):
     # 提出記録
     connect = sqlite3.connect("./server/DB/problem.db")
     cur = connect.cursor()
-    cur.execute("INSERT INTO submission VALUES(?, ?, ?, datetime(CURRENT_TIMESTAMP, \"+9 hours\"), ?, 0, \"\", 0)",
+    cur.execute("INSERT INTO submission VALUES(?, ?, ?, datetime(CURRENT_TIMESTAMP, \"+9 hours\"), ?, 0, \"\", 0, 0.0)",
                 (submission_id, user_id, problem_id, lang))
     connect.commit()
     cur.close()
